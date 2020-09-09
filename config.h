18ebdf8ea737104998af96495858822ba553a628
adamglw <53101365+adamglw@users.noreply.github.com>
2020-09-10 00:53:24 +0100

    Minor changes to colours

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+ 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -10,13 +10,13 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata:size=12" };
-static const char dmenufont[]       = "Inconsolata:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Hack Nerd font:size=10" };
+static const char dmenufont[]       = "Hack Nerd Font:size=10";
+static const char col_gray1[]       = "#32302f"; /* gruvbox bg0_s */
+static const char col_gray2[]       = "#504945"; /* gruvbox bg2 */
+static const char col_gray3[]       = "#bdae93"; /* gruvbox fg3 */
+static const char col_gray4[]       = "#ebdbb2"; /* gruvbox fg */
+static const char col_cyan[]        = "#478061"; /* void green */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
