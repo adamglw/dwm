33fe24fcca063ea00784cdfe4c4c1951b55af9a0
adamglw <53101365+adamglw@users.noreply.github.com>
2021-04-30 01:04:41 +0100

    Tweak colours

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
  
 /* appearance */
-static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int borderpx    = 1;        /* border pixel of windows */
 static const unsigned int snap        = 32;       /* snap pixel */
 static const unsigned int gappih      = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv      = 10;       /* vert inner gap between windows */
@@ -11,18 +11,17 @@ static const int smartgaps            = 0;        /* 1 means no outer gap when t
 static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const char *fonts[]            = { "Ubuntu Nerd Font:size=11" };
-static const char col_gray1[]         = "#1d2021"; /* #1d2021 = gbox dark bg0_h */
-static const char col_gray2[]         = "#282828"; /* #282828 = gbox dark bg0 */
-static const char col_gray3[]         = "#a89984"; /* #a89984 = gbox dark fg4 */
-static const char col_gray4[]         = "#d5c4a1"; /* #d5c4a1 = gbox dark fg2 */
-static const char col_cyan[]          = "#3c3936"; /* #3c3836 = gbox dark bg1 */
-static const char col_border[]        = "#ebddb2"; /* #ebddb2 = gbox fg */
+static const char col_gray1[]         = "#28282c"; /* dark gray */
+static const char col_gray2[]         = "#28282c"; /* #282828 = gbox dark bg0 */
+static const char col_gray3[]         = "#9d9da6"; 
+static const char col_gray4[]         = "#000000";
+static const char col_cyan[]          = "#836fa9"; /* soft violet */
 static const unsigned int baralpha    = 220;
 static const unsigned int borderalpha = 235; /* 235 = OPAQUE */
 static const char *colors[][3]        = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_border },
+	[SchemeSel]  = { col_gray4, col_cyan, col_cyan },
 };
 static const unsigned int alphas[][3] = {
 	/*               fg      bg        border   */
@@ -32,7 +31,7 @@ static const unsigned int alphas[][3] = {
 
 /* tagging */
 /* static const char *tags[] = { "", "", "", "", ""}; */
-static const char *tags[] = { "1", "2", "3", "4", "5"};
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7"};
 
 static const Rule rules[] = {
 	/* xprop(1):
